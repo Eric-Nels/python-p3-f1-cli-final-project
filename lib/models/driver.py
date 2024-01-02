fc0e07549ea909f612486854385eefa92c55b268
Eric Nelson <ernelson12o0@gmail.com>
2024-01-02 17:24:21 -0500

    add __repr__ to driver

--- lib/models/driver.py
+++ lib/models/driver.py
@@ -7,4 +7,10 @@ class Driver:
         self.name = name
         self.driver_role = driver_role
         self.team_id = team_id
+
+    def __repr__(self):
+        return (
+            f"<Driver {self.id}: {self.name}, {self.driver_role}, " +
+            f"Team ID: {self.team_id}>"
+        )
     
