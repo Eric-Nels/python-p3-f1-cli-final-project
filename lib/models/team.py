ff327076e3126e698a721e8c1aaab2e85a022918
Eric Nelson <ernelson12o0@gmail.com>
2024-01-02 17:36:29 -0500

    add name property and setter to team

--- lib/models/team.py
+++ lib/models/team.py
@@ -8,3 +8,14 @@ class Team:
 
     def __repr__(self):
         return f"<Team {self.id}: {self.name}, {self.location}>"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be non-empty string")
